6cbbfcd1289973ef5499f6e4fc768612c4291e8a
molek <molnar.adam995@gmail.com>
2023-10-16 00:54:44 +0200

    Create guess stream

--- src/app/game/components/solution-panel/solution-panel.component.ts
+++ src/app/game/components/solution-panel/solution-panel.component.ts
@@ -3,7 +3,7 @@ import { AppState } from '../../state/app.state';
 import { Store } from '@ngrx/store';
 import { selectPressedKeys } from '../../state/keyboard/keyboard.selector';
 import { selectSolution } from '../../state/game/game.selector';
-import { setSolutionWord } from '../../state/game/game.actions';
+import { map, withLatestFrom } from 'rxjs/operators';
 
 @Component({
   selector: 'app-solution-panel',
@@ -12,11 +12,31 @@ import { setSolutionWord } from '../../state/game/game.actions';
 })
 export class SolutionPanelComponent implements OnInit {
   readonly pressedKeys$  = this.store.select(selectPressedKeys)
-  readonly solution$ = this.store.select(selectSolution)
+  readonly solution$ = this.store.select(selectSolution).pipe(map(solution => {
+    const letters = solution.split('');
+    return letters.map(letter => ({
+      letter,
+      isCorrect: false
+    })) 
+  }))
+
+  readonly guessLetter$ = this.pressedKeys$.pipe(
+    withLatestFrom(this.solution$),
+    map(([pressedKeys, solution]) => {
+      console.log({pressedKeys, solution})
+      return solution.map(s => {
+        if (pressedKeys.includes(s.letter)) {
+          s.isCorrect = true
+        }
+        return s
+      })
+    }),
+  )
 
   constructor(private store: Store<AppState>) {}
 
   ngOnInit(): void {
     this.pressedKeys$.subscribe(k => console.log({k}))
+    this.guessLetter$.subscribe(guess => console.log({guess}))
   }
 }
